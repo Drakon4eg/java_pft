6c54566d3381b78ad0e369e767aa7436bd20cfe9
Drakon4eg <Dreik47@ya.ru>
2020-09-28 16:27:19 +0300

    функции

--- sandbox/src/main/java/ru/stqa/pft/sandox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandox/MyFirstProgram.java
@@ -3,7 +3,28 @@ package ru.stqa.pft.sandox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Fuck, world");
+    hello("world");
+    hello("user");
+    hello("bitch");
+
+    double l = 5;
+      System.out.println("Площадь квадрата со стороной " + l + " = " +area(l));
+
+      double a = 4;
+      double b = 6;
+    System.out.println("Площадт прямогу со сторонами " + a +" " + b + " = " + area(a,b));
+  }
+
+  public static void hello( String somebody) {
+      System.out.println("Fuck, "+ somebody +"!");
+  }
+
+  public static double area(double len) {
+      return len * len;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
   }
 
 }
